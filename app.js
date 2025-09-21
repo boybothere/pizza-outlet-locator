ed90e8eb7ecbb7b308cbf26629cb6e642dabe84f
Adrian Fernandes <boybothere@gmail.com>
2025-09-22 00:33:21 +0530

    add store list interaction and flyTo functionality

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ function generateList() {
         a.innerText = shop.properties.name;
         a.href = '#';
         p.innerText = shop.properties.address;
-
+        a.addEventListener('click', () => {
+            flyToStore(shop)
+        });
         div.appendChild(a);
         div.appendChild(p);
         li.appendChild(div);
@@ -29,3 +31,50 @@ function generateList() {
 }
 
 generateList();
+
+function makePopup(shop) {
+    return `
+        <div>
+            <h4>${shop.properties.name}</h4>
+            <p>${shop.properties.address}</p>
+            <div class="phone-number">
+                <a href="tel:${shop.properties.phone}">${shop.properties.phone}</a>
+            </div>
+        </div>
+    `
+}
+
+function onEachFeature(feature, layer) {
+    layer.bindPopup(makePopup(feature), { closeButton: false, offset: L.point(0, -8) });
+}
+
+const myIcon = L.icon({
+    iconUrl: './marker.png',
+    iconSize: [15, 25],
+})
+
+const shopsLayer = L.geoJSON(storeList, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, { icon: myIcon })
+    }
+})
+
+shopsLayer.addTo(map);
+
+function flyToStore(store) {
+    const lat = store.geometry.coordinates[1];
+    const lng = store.geometry.coordinates[0];
+    map.flyTo([lat, lng], 14, {
+        duration: 3
+    });
+
+    setTimeout(() => {
+        L.popup({ closeButton: false, offset: L.point(0, -8) })
+            .setLatLng([lat, lng])
+            .setContent(makePopup(store))
+            .openOn(map);
+    }, 3000);
+}
+
+
